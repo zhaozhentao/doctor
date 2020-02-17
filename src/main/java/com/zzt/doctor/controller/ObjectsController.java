ddc8821f3e96aad1fda541b50f1c861d23631f75
zhaozhentao <344696734@qq.com>
2020-02-17 08:14:00 +0800

    object detail

--- src/main/java/com/zzt/doctor/controller/ObjectsController.java
+++ src/main/java/com/zzt/doctor/controller/ObjectsController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -44,8 +46,9 @@ public class ObjectsController {
         if (thing == null) {
             return null;
         } else if (thing instanceof JavaClass) {
-            return "";
+            return printFullClass((JavaClass) thing, snapshot);
         } else if (thing instanceof JavaValueArray) {
+            map.put("type", "Value");
             map.put("isValue", true);
             map.put("value", ((JavaValueArray) thing).valueString(true));
             map.put("referencesTo", referencesTo(thing, snapshot));
@@ -55,6 +58,7 @@ public class ObjectsController {
         } else if (thing instanceof JavaObject) {
             ObjectInstance instance = new ObjectInstance(thing.toString(), thing.getClazz().getIdString(), thing.getSize());
 
+            map.put("type", "JavaObject");
             map.put("instance", instance);
             map.put("class", thing.getClazz().getName());
             map.put("members", objectMembers((JavaObject) thing));
@@ -65,6 +69,128 @@ public class ObjectsController {
             // We should never get here
             return "";
         }
+    }
+
+    private Object printFullClass(JavaClass clazz, Snapshot snapshot) {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("type", "JavaClass");
+
+        result.put("className", clazz.toString());
+
+        result.put("Superclass", printClass(clazz.getSuperclass()));
+
+        result.put("ClassLoader", thingDetail(clazz.getLoader(), snapshot));
+
+        result.put("Signers", thingDetail(clazz.getSigners(), snapshot));
+
+        result.put("Protection Domain", thingDetail(clazz.getProtectionDomain(), snapshot));
+
+        ArrayList<Object> subclasses = new ArrayList<>();
+        result.put("Subclasses", subclasses);
+        JavaClass[] sc = clazz.getSubclasses();
+        for (JavaClass javaClass : sc) {
+            subclasses.add(printClass(javaClass));
+        }
+
+        ArrayList<Object> instanceMembers = new ArrayList<>();
+        result.put("Instance Data Members", instanceMembers);
+        JavaField[] ff = clazz.getFields().clone();
+        ArraySorter.sort(ff, new Comparer() {
+            @Override
+            public int compare(Object lhs, Object rhs) {
+                JavaField left = (JavaField) lhs;
+                JavaField right = (JavaField) rhs;
+                return left.getName().compareTo(right.getName());
+            }
+        });
+        for (JavaField javaField : ff) {
+            instanceMembers.add(printField(javaField));
+        }
+
+        ArrayList<Object> staticMembers = new ArrayList<>();
+        result.put("Static Data Members", staticMembers);
+        JavaStatic[] ss = clazz.getStatics();
+        for (JavaStatic s : ss) {
+            staticMembers.add(printStatic(s, snapshot));
+        }
+
+        result.put("instances", instances(clazz, snapshot));
+
+//        if (snapshot.getHasNewSet()) {
+//            out.println("<h2>New Instances</h2>");
+//
+//            printAnchorStart();
+//            print("newInstances/" + encodeForURL(clazz));
+//            out.print("\">");
+//            out.println("Exclude subclasses</a><br>");
+//
+//            printAnchorStart();
+//            print("allNewInstances/" + encodeForURL(clazz));
+//            out.print("\">");
+//            out.println("Include subclasses</a><br>");
+//        }
+
+        result.put("referencesToThisObject", referencesTo(clazz, snapshot));
+
+        return result;
+    }
+
+    protected Object printStatic(JavaStatic member, Snapshot snapshot) {
+        HashMap<String, Object> result = new HashMap<>(4);
+
+        JavaField f = member.getField();
+        result.put("field", printField(f));
+        if (f.hasId()) {
+            JavaThing t = member.getValue();
+            result.put("objectValue", thingDetail(t, snapshot));
+        } else {
+            result.put("value", member.getValue().toString());
+        }
+
+        return result;
+    }
+
+    private Object instances(JavaClass clazz, Snapshot snapshot) {
+        ArrayList<Object> instances = new ArrayList<>();
+
+        Enumeration objects = clazz.getInstances(false);
+        while (objects.hasMoreElements()) {
+            JavaHeapObject obj = (JavaHeapObject) objects.nextElement();
+
+            instances.add(thingDetail(obj, snapshot));
+        }
+
+        return instances;
+    }
+
+    protected Object printClass(JavaClass clazz) {
+        if (clazz == null) {
+            return null;
+        }
+
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("class", clazz.toString());
+        result.put("classAddress", encodeForURL(clazz));
+
+        return result;
+    }
+
+    protected String encodeForURL(JavaClass clazz) {
+        if (clazz.getId() == -1) {
+            return encodeForURL(clazz.getName());
+        } else {
+            return clazz.getIdString();
+        }
+    }
+
+    protected String encodeForURL(String s) {
+        try {
+            s = URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+            // Should never happen
+            ex.printStackTrace();
+        }
+        return s;
     }
 
     protected Object referencesTo(JavaHeapObject obj, Snapshot snapshot) {
@@ -134,6 +260,15 @@ public class ObjectsController {
         }
 
         result.put("valueString", thing.toString());
+        return result;
+    }
+
+    protected Object printField(JavaField field) {
+        HashMap<String, Object> result = new HashMap<>(4);
+
+        result.put("fieldName", field.getName());
+        result.put("signature", field.getSignature());
+
         return result;
     }
 
